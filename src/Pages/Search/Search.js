c296e0fef6f9ebf08d842306d085cb7198b0cd67
Mahmudulk675 <mahmudulk675@gmail.com>
2021-06-17 18:32:19 +0600

    search-fixed

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -32,9 +32,9 @@ const Search = () => {
   const fetchSearch = async () => {
     try {
       const { data } = await axios.get(
-        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
-          process.env.REACT_APP_API_KEY
-        }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+        `https://api.themoviedb.org/3/search/${
+          type ? "tv" : "movie"
+        }?api_key=ff0e8697ca055c126dec3747cfaa421f&language=en-US&query=${searchText}&page=${page}&include_adult=false`
       );
       setContent(data.results);
       setNumOfPages(data.total_pages);
